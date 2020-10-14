d5e47c7751a77bd0627665bb0e1b5535da1bbce9
cathjime <xcatherine.jimenez@gmail.com>
2020-10-14 13:02:23 -0400

    jwt backend setup complete not tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,46 @@
 class UsersController < ApplicationController
+    before_action :authorized, only: [:auto_login]
     def index
         users = User.all
         render json:users 
 
     end
+
+    # REGISTER
     def create
-        user= User.create(user_params)
-        render json:user
+        user = User.create(user_params)
+        if user.valid?
+          token = encode_token({user_id: user.id})
+          render json: {user: user, token: token}
+        else
+          render json: {error: "Invalid username or password"}
+        end
     end
+
     def update
         user = User.find(params[:id])
         user.update(user_params)
         render json:user
     end
+
+    # LOGGING IN
+    def login
+        user = User.find_by(username: params[:username])
+
+        if user && user.authenticate(params[:passsword_digest])
+            token = encode_token({user_id: user.id})
+            render json: {user: user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
+    end
+
+
+    def auto_login
+        render json: user
+    end
+
+  private
   
     private
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create]
+  post '/login', to: "users#login"
+  get '/auto_login', to: "users#auto_login"
   resources :favorites
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
